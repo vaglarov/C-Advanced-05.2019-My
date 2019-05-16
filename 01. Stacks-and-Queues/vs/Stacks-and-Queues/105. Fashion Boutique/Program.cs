95ba670ecab813dfe181070dc00b42e2ba64f464
vaglarov <pasific@abv.bg>
2019-05-16 19:49:46 +0300

    fashionBoutique

---
+++ 01. Stacks-and-Queues/vs/Stacks-and-Queues/105. Fashion Boutique/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _105._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputAllDressInLine = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Reverse().ToArray();
+            var stackBoxx = new Stack<int>();
+            var numberDressInBox = int.Parse(Console.ReadLine());
+            var boxField = 0;
+            for (int i = 0; i < inputAllDressInLine.Length; i++)
+            {
+                boxField += inputAllDressInLine[i];
+                if (boxField >= numberDressInBox)
+                {
+                    stackBoxx.Push(numberDressInBox);
+                    boxField -= numberDressInBox;
+
+                    if (boxField >= 0)
+                    {
+                        stackBoxx.Push(boxField);
+                    boxField = 0;
+                    }
+                }
+				else
+            }
+            Console.WriteLine(stackBoxx.Count);
+        }
+    }
+}
