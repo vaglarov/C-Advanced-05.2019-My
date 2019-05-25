d9a68864c4c681248e4da9f7342755fd16dac0a5
vaglarov <pasific@abv.bg>
2019-05-25 22:25:10 +0300

    new

---
+++ 03. Sets-and-Dictionaries-Advanced/vs/SetsAndDictionaries/107. The V-Logger/TheVLogger.cs
@@ -0,0 +1,172 @@
+﻿namespace _107._The_V_Logger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class VLogger
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                var dictVlogers = new Dictionary<string, Vloger>();
+                var input = string.Empty;
+                //Read Input
+                while ((input = Console.ReadLine()) != "Statistics")
+                {
+                    var splitInput = input.Split();
+                    var nameVloger = string.Empty;
+                    switch (splitInput[1])
+                    {
+                        case "joined":
+                            nameVloger = splitInput[0];
+                            if (!dictVlogers.ContainsKey(nameVloger))
+                            {
+                                var newVloger = new Vloger(nameVloger);
+                                dictVlogers.Add(nameVloger, newVloger);
+                            }
+                            break;
+                        case "followed":
+                            var nameReglarPersonFirst = splitInput[0];
+                            var nameVlogerSecond = splitInput[2];
+
+                            if (nameReglarPersonFirst == nameVlogerSecond)
+                            {
+                                break;
+                            }
+                            if (dictVlogers.ContainsKey(nameVlogerSecond) && dictVlogers.ContainsKey(nameReglarPersonFirst))
+                            {
+                                var vloger = dictVlogers[nameVlogerSecond];
+                                //dobawamq imenata 
+                                if (!vloger.ContainsFollowers(nameReglarPersonFirst))
+                                {
+                                    vloger.AddFollowersName(nameReglarPersonFirst);
+                                }
+                                //dobawqme na vlogera followers
+
+                                var fowoling = dictVlogers[nameReglarPersonFirst];
+                                if (!fowoling.ContainsFollowing(nameVlogerSecond))
+                                {
+                                    fowoling.AddFollowingName(nameVlogerSecond);
+                                }
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Outher type of input");
+                            break;
+
+                    }
+                    //PrintStatistic(dictVlogers);
+                }
+
+                //Print Statistic
+
+                PrintStatistic(dictVlogers);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private static void PrintStatistic(Dictionary<string, Vloger> dictVlogers)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            var counter = 1;
+            Console.WriteLine($"The V-Logger has a total of {dictVlogers.Count} vloggers in its logs.");
+            foreach (var vloger in dictVlogers.
+                OrderByDescending(x => x.Value.FollowersCount()).
+                ThenBy(x => x.Value.FollowingCount()))
+            {
+                if (counter == 1)
+                {
+                    Console.WriteLine($"{counter}. {vloger.Key} : {vloger.Value.FollowersCount()} followers, {vloger.Value.FollowingCount()} following");
+                    var listFollowers = vloger.Value.ListFollowers();
+                    foreach (var liker in listFollowers.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"*  {liker}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{counter}. {vloger.Key} : {vloger.Value.FollowersCount()} followers, {vloger.Value.FollowingCount()} following");
+                }
+                counter++;
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+    class Vloger
+    {
+        public string vlogerName;
+        public List<string> followers;
+        public List<string> following;
+
+        public Vloger(string name)
+        {
+            this.Name = name;
+            this.Followers = new List<string>();
+            this.Following = new List<string>();
+        }
+
+
+        //following 
+        public bool ContainsFollowing(string followingName)
+        {
+            if (!this.Following.Contains(followingName))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void AddFollowingName(string followingName)
+        {
+                this.Following.Add(followingName);
+        }
+
+        public List<string> ListFollowing()
+        {
+            return this.Following;
+        }
+        public int FollowingCount()
+        {
+            var result = this.Following.Count;
+            return result;
+        }
+
+        //following 
+        public bool ContainsFollowers(string followingName)
+        {
+            if (this.Followers.Contains(followingName))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void AddFollowersName(string followingName)
+        {
+            this.Followers.Add(followingName);
+        }
+
+        public List<string> ListFollowers()
+        {
+            return this.Followers;
+        }
+        public int FollowersCount()
+        {
+            return this.Followers.Count;
+        }
+
+        public string Name { get; set; }
+        public List<string> Followers { get; set; }
+        public List<string> Following { get; set; }
+    }
+
+}
---
+++ 03. Sets-and-Dictionaries-Advanced/vs/SetsAndDictionaries/108. Ranking/Ranking.cs
@@ -0,0 +1,110 @@
+﻿namespace _108._Ranking
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Ranking
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Dictionary<string, string> dictContest = new Dictionary<string, string>();
+                Dictionary<string, Student> dictStudent = new Dictionary<string, Student>();
+                string input = string.Empty;
+                //Read constest with password
+                while ((input = Console.ReadLine()) != "end of contests")
+                {
+                    string[] inputSplit = input.Split(":", StringSplitOptions.RemoveEmptyEntries);
+                    string contest = inputSplit[0];
+                    string passwordContest = inputSplit[1];
+                    if (!dictContest.ContainsKey(contest))
+                    {
+                        dictContest.Add(contest, passwordContest);
+                    }
+                }
+                //Read student With Submissions
+                while ((input = Console.ReadLine()) != "end of submissions")
+                {
+                    string[] inputSplit = input.Split("=>", StringSplitOptions.RemoveEmptyEntries);
+                    string contest = inputSplit[0];
+                    string passwordContest = inputSplit[1];
+                    string nameStudent = inputSplit[2];
+                    int pointContest = int.Parse(inputSplit[3]);
+                    if (dictContest.ContainsKey(contest) && dictContest[contest] == (passwordContest))
+                    {
+                        if (!dictStudent.ContainsKey(nameStudent))
+                        {
+                            var student = new Student(nameStudent);
+                            student.AddContest(contest, pointContest);
+                            dictStudent.Add(nameStudent, student);
+                        }
+                        else
+                        {
+                            var student = dictStudent[nameStudent];
+                            student.AddContest(contest, pointContest);
+                        }
+                    }
+                }
+                Print(dictStudent);
+                
+            }
+            catch (Exception ex)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        private static void Print(Dictionary<string, Student> dictStudent)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            var bestStudent = dictStudent.OrderByDescending(x => x.Value.TotalResult).First();
+            Console.WriteLine($"Best candidate is {bestStudent.Key} with total {bestStudent.Value.TotalResult} points.");
+            Console.WriteLine("Ranking:");
+            foreach (var kvp in dictStudent.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}");
+                foreach (var contestResult in kvp.Value.ResultStudent())
+                {
+                    Console.WriteLine($"#  {contestResult.Key} -> {contestResult.Value}");
+                }
+            }
+        }
+    }
+
+    public class Student
+    {
+        public Dictionary<string, int> dataContestResult;
+        public int TotalResult { get => this.dataContestResult.Sum(x => x.Value); set => this.dataContestResult.Sum(x => x.Value); }
+        public int totalResult;
+        public Student(string name)
+        {
+            this.Name = name;
+            this.dataContestResult = new Dictionary<string, int>();
+            this.TotalResult = this.totalResult;
+        }
+
+        public void AddContest(string contest,int point)
+        {
+            if (!this.dataContestResult.ContainsKey(contest))
+            {
+                this.dataContestResult.Add(contest, point);
+            }
+            else if (this.dataContestResult[contest]<point)
+            {
+                this.dataContestResult[contest] = point;
+            }
+        }
+
+        public Dictionary<string,int> ResultStudent()
+        {
+            var result = this.dataContestResult.OrderByDescending(x => x.Value).ThenBy(x=>x.Key).ToDictionary(x=>x.Key,y=>y.Value);
+            return result;
+        }
+        public string Name { get; private set; }
+    }
+}
+ 
